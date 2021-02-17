e1af8c3bd7574f4ddec0405f7704084fb6293e5e
Mikhaila Lynn <mikhailalynn@gmail.com>
2021-02-16 16:21:30 -0800

    Finished assignment

---
+++ js/script.js
@@ -0,0 +1,191 @@
+// STEP 1
+/* let some_month;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+*/
+
+// STEP 2
+/*numeric literal expression: 12 
+string literal expression: "Hello world!" 
+Boolean literal expression: true
+null literal expression: null 
+*/
+
+
+// STEP 3
+/* let averageScore = score1 + score2;
+var name = "Mikhaila";
+*/
+
+//STEP 4
+/*
+let firstName;
+let lastName;
+let address;
+let city;
+let state;
+let zipCode;
+let yourAge;
+let referralSource;
+let mayWeContactYou;
+*/
+
+//STEP 5
+/*
+let firstName; 
+firstName = "Mikhaila"; 
+
+let firstName = "Mikhaila";
+
+const firstName = "Mikhaila";
+
+
+let state;
+state = "California";
+
+let state = "California";
+
+const state = "California";
+
+
+let yourAge;
+yourAge = "26";
+
+let yourAge = "26";
+
+const yourAge = "26";
+*/
+
+//STEP 6
+/* let caseQuantity = 24;
+console.log("Each case contains " + caseQuantity + " boxes.");
+*/
+
+//STEP 7
+/*
+let theSky = "The color of the sky is blue...";
+console.log(theSky + true);
+
+let x = 5;
+console.log(x + false);
+*/
+
+//STEP 8
+/*
+The string in this example is not valid because of the incorrectly placed quotation marks- it needs backslashes to indicate that the quotations within the string are characters, and not terminators. It should be:
+
+let someString = "Who once said, \'Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\'";
+window.console.log(someString);
+*/
+
+//STEP 9
+/* let x = 200;
+x = null;
+alert(x);
+
+let y;
+alert(y);
+*/
+
+//STEP 10
+/*
+console.log(typeof 6);
+console.log(typeof "Hello world");
+console.log(typeof false);
+console.log(typeof {x:1, y:2} );
+console.log(typeof undefined);
+*/
+
+//STEP 11
+/* let x = "Hello";
+let y = " Mikhaila Lynn";
+let z = ", welcome to the JavaScript class!"
+alert(x + y + z);
+*/
+
+//STEP 12
+/*
+let x = "Hello ";
+let myName = "Mikhaila";
+let z = ", welcome to the JavaScript class!"
+alert(x + myName + z);
+*/
+
+//STEP 13
+/*
+let hello = "Hello ";
+let myName = "Mikhaila";
+let welcomeMssgStart = ", welcome to the "
+let course = "JavaScript ";
+let welcomeMssgEnd = "class!"
+alert(hello + myName + welcomeMssgStart + course + welcomeMssgEnd);
+*/
+
+//STEP 14
+/*
+let hello = "Hello ";
+let myName = "Mikhaila. \n";
+let welcomeMssgStart = "Welcome to the "
+let course = "JavaScript ";
+let welcomeMssgEnd = "class!"
+alert(hello + myName + welcomeMssgStart + course + welcomeMssgEnd);
+*/
+
+//STEP 15
+/*
+let hello = "Hello ";
+let yourName = prompt("Your name?");
+let welcomeMssgStart = " Welcome to the "
+let course = "JavaScript ";
+let welcomeMssgEnd = "class!"
+alert(hello + yourName + "." + welcomeMssgStart + course + welcomeMssgEnd);
+*/
+
+//STEP 16
+/*let hello = "Hello ";
+let yourName = prompt("Your name?");
+let welcomeMssgStart = " Welcome to the "
+let course = prompt("What class are you taking?");
+let welcomeMssgEnd = " class!"
+alert(hello + yourName + "." + welcomeMssgStart + course + welcomeMssgEnd);
+*/
+
+//STEP 17
+/*
+let x = 10;
+let y = 20;
+console.log(x + y);
+*/
+
+//STEP 18 
+/*
+let x = 20;
+console.log (x + 20); */
+
+//STEP 19
+/*
+let x = 20;
+console.log (x *= 5);
+*/
+
+//STEP 20
+/*
+let x = 20 / 3;
+console.log (x/= 1);
+*/
+
+//STEP 21
+/*
+let x = "apples";
+let y = "oranges";
+alert(x == "apples" && y == "oranges");
+*/
+
+//STEP 22
+/*
+let x = 1;
+let y = 10;
+alert(x > y || x >= y);
+*/
